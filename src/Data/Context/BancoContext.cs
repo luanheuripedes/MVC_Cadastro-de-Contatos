445db9ebae2094f7373734b5d7493b2fdfac09ee
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-01-17 15:59:44 -0300

    Criado a camada de dados e adicionado o EF

---
+++ src/Data/Context/BancoContext.cs
@@ -0,0 +1,21 @@
+﻿
+using ControleDeContatos.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class BancoContext:DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options):base(options)
+        {
+        }
+
+        public DbSet<ContatoEntitie> Contatos { get; set; }
+
+    }
+}
---
+++ src/Data/Entities/ContatoEntitie.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleDeContatos.Entities
+{
+    public class ContatoEntitie
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+    }
+}
